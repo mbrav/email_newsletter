39070434cce0f2140eadcb64840cdf5094055049
mbrav <mbrav@protonmail.com>
2022-08-26 20:57:51 +0000

    Add clonable db connection based on Arc

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,18 @@
 use crate::routes::{greet, health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server: Server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server: Server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
             .route("/", web::get().to(greet))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
