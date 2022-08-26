cc01e087725635774c3a6bc13e10f68cbaee84f0
mbrav <mbrav@protonmail.com>
2022-08-26 09:58:52 +0300

    Add serde and status code tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use serde;
 use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -9,6 +16,10 @@ async fn health_check() -> HttpResponse {
 async fn greet(req: HttpRequest) -> impl Responder {
     let name: &str = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 // We need to mark `run` as public.
@@ -20,10 +31,11 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/{name}", web::get().to(greet))
     })
     .listen(listener)?
     .run();
-    println!("Running server on http://127.0.0.1:8000");
+
     Ok(server)
 }
